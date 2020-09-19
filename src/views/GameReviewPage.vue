67046ad38c1a2365f9c2c59181c6162f82a75490
6110406139 <pichathat.s@ku.th>
2020-09-19 16:32:04 +0700

    add RatingForm

--- src/views/GameReviewPage.vue
+++ src/views/GameReviewPage.vue
@@ -1,13 +1,36 @@
 <template>
-  <p>GameReviewPage</p>
+  <div>
+    <h1>
+      GameReviewPage
+    </h1>
+    <p>GameName</p>
+    <li>Story</li>
+    <RatingStar />
+    <br>
+    <li>Gameplay</li>
+    <RatingStar />
+    <br>
+    <li>Graphics</li>
+    <RatingStar />
+    <br>
+    <li>Performance</li>
+    <RatingStar />
+    <br>
+    <li>Sound</li>
+    <RatingStar />
+  </div>
+  
 </template>
 
 <script>
+import RatingStar from '../components/GameReviewComponent/RatingForm'
 export default {
-
+  components: {
+    RatingStar
+  }
 }
 </script>
 
-<style>
 
+<style>
 </style>
