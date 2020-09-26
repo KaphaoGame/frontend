5ca1733d70badad2687f47468aae64df12ced156
Nuttawatt Sunthornsatienkul <Nuttawatt.s@ku.th>
2020-09-26 15:07:57 +0700

    Change GameDetailPage.vue and add ScoresCard.vue and UserReviewsCard.vue in gameDetailComponent

---
+++ src/components/gameDetailComponent/UserReviewsCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <div align="center">
+          <div id="mock-score">
+            <b-avatar src="https://placekitten.com/300/300" size="4rem"></b-avatar>
+            <span> {{score}}</span>
+          </div>
+          <div>
+            <div class="specific-score" align="start">
+                <span>Story</span>
+                <br>
+                <span>Gameplay</span>
+                <br>
+                <span>Graphics</span>
+                <br>
+                <span>Performance</span>
+                <br>
+                <span>Sound</span>
+                <br>
+            </div>
+          </div>
+        </div>
+      </b-col>
+
+      <b-col>
+        <div>
+          <div class="review-card">
+            <span class="font-style2">{{username}}</span>
+            <br>
+            <span>{{reviewParagraph}}</span>
+
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+    props: [
+    'username',
+    'score',
+    'reviewParagraph'
+  ],
+  data() {
+      return {
+        animate: true
+      }
+    } 
+};
+</script>
+
+<style>
+#mock-score {
+  font-size: 2vw;
+  color: #e9ecef;
+  background: #18850c;
+  border-radius: 7px;
+  width: 150px;
+}
+
+.review-card {
+  background: #bbc5ce;
+  width: 400px;
+  height: 200px;
+}
+
+.specific-score {
+  background: #ffffff;
+  width: 150px;
+  height: 120px;
+}
+
+.font-style2 {
+    font-size: 3.0vw;
+}
+
+</style>
--- src/views/GameDetailPage.vue
+++ src/views/GameDetailPage.vue
@@ -1,36 +1,130 @@
 <template>
-  <div id="gameDetail">
-    <button @click="dataAction">get Game from API</button> <br>
+  <div class="gameDetail container">
+    <h1 class="font-style">Game Name</h1>
+    <br />
+
+    <b-row class="mb-3">
+      <b-img
+        src="https://uknow.in.th/wp-content/uploads/2020/03/7-7.jpg"
+        width="300px"
+        height="540px"
+      />
+      <b-col><ScoresCard title="Game Scores card" /></b-col>
+    </b-row>
+    
+
+    <div class="col-8">
+
+      <div id="tag-card">TAG: RTS</div>
+      <br />
+
+      <b-row id="description-card">
+        <h3>Description</h3>
+        <p>
+          I don't know what to write paragraph. I don't know what to write
+          paragraph. I don't know what to write paragraph. I don't know what
+          to write paragraph. I don't know what to write paragraph. I don't
+          know what to write paragraph. I don't know what to write
+          paragraph.
+        </p>
+      </b-row>
+
+      <br />
+      <b-row id="user-reviews-card">
+        <h3>User Reviews</h3>
+          <UserReviewsCard username="Kendo" score="9.0" reviewParagraph="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" />
+        <br>
+          <UserReviewsCard username="Kendos" score="8.8" reviewParagraph="bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+          bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+          bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+          bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" />
+        <br>
+          <UserReviewsCard username="KendosOwO" score="9.5" reviewParagraph="cccccccccccccccccccccccccccccccccccccccccccccccccccc
+          cccccccccccccccccccccccccccccccccccccccccccccccccccc
+          cccccccccccccccccccccccccccccccccccccccccccccccccccc
+          cccccccccccccccccccccccccccccccccccccccccccccccccccc" />
+        <br>
+      </b-row>
+    </div>
+    
+    <br> 
+    
+    <div id="recommend-card" class="col-4">Recommend</div>
+      
+    
+    <br>
+
+    <!-- <button @click="dataAction">get Game from API</button> <br>
     <div>
       <h1>{{$store.state.data.gameName}}</h1>
       <img :src= $store.state.data.gameImageURL>
       <h3>{{$store.state.data.gameReleaseDate}}</h3>
       <h3>{{$store.state.data.gameDescription_raw}}</h3>
-
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+import ScoresCard from "../components/gameDetailComponent/ScoresCard";
+import UserReviewsCard from "../components/gameDetailComponent/UserReviewsCard";
 
 export default {
-  name: 'gamedetail',
-  methods: {
-    dataAction(){
-      this.$store.dispatch("dataAction")
-    },
+  name: "gamedetail",
+
+  components: {
+    ScoresCard,
+    UserReviewsCard,
   },
 
-}
+  methods: {
+    dataAction() {
+      this.$store.dispatch("dataAction");
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#tag-card {
+  font-size: 2vw;
+  color: grey;
+  background: #e8eaec;
+  width: 640px;
+  border-radius: 10px;
+}
+
+#description-card {
+  background: #e8eaec;
+  width: 640px;
+  border-radius: 10px;
+}
+
+#user-reviews-card {
+  background: #e8eaec;
+  width: 640px;
+  border-radius: 10px;
+}
+
+#recommend-card {
+  background: #e8eaec;
+  width: 100px;
+  border-radius: 10px;
+}
+
+.font-style {
+  font-size: 1.2vw;
+  color: #e9ecef;
 }
 </style>
