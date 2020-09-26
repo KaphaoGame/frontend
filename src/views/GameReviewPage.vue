a4787c7b22ce947b74808ab7494ddbdb44a9af79
6110402818 <48822658+6110402818@users.noreply.github.com>
2020-09-26 15:04:25 +0700

    Decorate GameReview Page

--- src/views/GameReviewPage.vue
+++ src/views/GameReviewPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <br />
     <div class="font-style-1">
       <h1>GameReviewPage</h1>
     </div>
@@ -26,16 +27,16 @@
         <!-- Text slides with image -->
         <b-carousel-slide
           caption="First slide"
-          img-src="https://i.ytimg.com/vi/S1WmmsEXtyE/maxresdefault.jpg"
+          img-src="https://www.gensh.in/wallpaper/genshin/genshin_16.png"
         ></b-carousel-slide>
 
         <!-- Slides with custom text -->
-        <b-carousel-slide img-src="https://i.ytimg.com/vi/S1WmmsEXtyE/maxresdefault.jpg">
+        <b-carousel-slide img-src="https://www.gensh.in/wallpaper/genshin/genshin_17_4K.jpg">
           <h1>Hello world!</h1>
         </b-carousel-slide>
 
         <!-- Slides with image only -->
-        <b-carousel-slide img-src="https://i.ytimg.com/vi/S1WmmsEXtyE/maxresdefault.jpg"></b-carousel-slide>
+        <b-carousel-slide img-src="https://www.gensh.in/wallpaper/genshin/genshin_19_4K.jpg"></b-carousel-slide>
 
         <!-- Slides with img slot -->
         <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
@@ -45,7 +46,7 @@
               class="d-block img-fluid w-100"
               width="1024"
               height="480"
-              src="https://i.ytimg.com/vi/S1WmmsEXtyE/maxresdefault.jpg"
+              src="https://www.gensh.in/wallpaper/genshin/genshin_06_4K.jpg"
               alt="image slot"
             />
           </template>
@@ -100,13 +101,18 @@
         ></b-form-textarea>
 
         <pre class="mt-3 mb-0">{{ text }}</pre>
+     
       </div>
       <div class="d-flex flex-row-reverse">
         <router-link to="gamedetail">
-          <b-button variant="primary">SUBMIT</b-button>
+          
+
+          <button class="btn-submit">SUBMIT</button>
+          
         </router-link>
       </div>
     </div>
+    <br />
   </div>
 </template>
     
@@ -150,5 +156,11 @@ export default {
 .font-style-3 {
   font-size: 1.5vw;
   color: #e8eaec;
+}
+
+.btn-submit{
+  
+  background-color: #FF6657;
+  border-radius: 20%;
 }
 </style>
